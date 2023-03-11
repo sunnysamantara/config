91c666ce1234117b0e583a7b5f714640abb129ea
Sunny Samantara <35838456+sunnysamantara@users.noreply.github.com>
2023-03-11 12:45:57 +0530

    Update ArchKDE.sh

--- ArchKDE.sh
+++ ArchKDE.sh
@@ -20,13 +20,14 @@ cd ..
 #application
 sudo pacman -S --noconfirm yakuake kde-gtk-config ark dolphin dolphin-plugins gwenview kdeconnect neofetch  neovim packagekit-qt5 partitionmanager spectacle kate vlc firefox ktorrent gimp bpytop bitwarden ufw reflector avahi kcalc xdg-utils xdg-user-dirs bash-completion bash-language-server  	openssh kamoso kdenlive kdegraphics-thumbnailers kimageformats libheif qt5-imageformats kdesdk-thumbnailers ffmpegthumbs taglib variety okular kvantum android-tools appmenu-gtk-module libreoffice-fresh elisa
 
-paru -S --noconfirm resvg raw-thumbnailer kde-thumbnailer-apk xdman protonvpn google-chrome systemd-kcm kio-admin qtscrcpy
+paru -S --noconfirm resvg raw-thumbnailer kde-thumbnailer-apk xdman protonvpn google-chrome systemd-kcm kio-admin qtscrcpy auto-cpufreq
 
 echo "exec startkde" >> ~/.xinitrc
 sudo ufw allow 1714:1764/udp
 sudo ufw allow 1714:1764/tcp
 sudo systemctl enable sddm
 sudo systemctl enable reflector.timer
+sudo systemctl enable auto-cpufreq
 sudo systemctl enable power-profiles-daemon.service
 sudo systemctl enable avahi-daemon
 sudo systemctl enable bluetooth
