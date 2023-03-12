bd6572e54e95f616b427c21a2559f18a93e8ea08
Sunny Samantara <35838456+sunnysamantara@users.noreply.github.com>
2023-03-12 11:33:52 +0530

    Update ArchKDE.sh

--- ArchKDE.sh
+++ ArchKDE.sh
@@ -5,10 +5,13 @@ sudo timedatectl set-timezone Asia/Kolkata
 sudo pacman -Syy
 
 #basic drivers and sessions
-sudo pacman -S --noconfirm  xorg xorg-xinit xf86-video-intel nvidia-settings nvidia-prime nvidia nvidia-utils sddm sddm-kcm plasma plasma-desktop plasma-wayland-session plasma-wayland-protocols bluez power-profiles-daemon encfs powerdevil wpa_supplicant konsole intel-ucode git
+sudo pacman -S --noconfirm  xorg xorg-xinit xf86-video-intel nvidia-settings nvidia-prime nvidia nvidia-utils sddm sddm-kcm plasma plasma-desktop plasma-wayland-session plasma-wayland-protocols bluez encfs powerdevil wpa_supplicant konsole intel-ucode git
 
 #audio
-sudo pacman -S --noconfirm pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-media-session qpwgraph pipewire-zeroconf
+sudo pacman -S --noconfirm pipewire pipewire-alsa pipewire-audio pipewire-pulse pipewire-jack wireplumber qpwgraph
+
+#application
+sudo pacman -S --noconfirm yakuake kde-gtk-config ark dolphin dolphin-plugins gwenview kdeconnect neofetch  neovim packagekit-qt5 partitionmanager spectacle kate vlc firefox ktorrent gimp bpytop bitwarden ufw reflector avahi kcalc xdg-utils xdg-user-dirs bash-completion bash-language-server  	openssh kamoso kdenlive kdegraphics-thumbnailers kimageformats libheif qt5-imageformats kdesdk-thumbnailers ffmpegthumbs taglib variety okular kvantum android-tools appmenu-gtk-module libreoffice-fresh elisa
 
 #paru aur helper
 git clone https://aur.archlinux.org/paru.git
@@ -16,11 +19,7 @@ cd paru
 makepkg -si --noconfirm
 cd ..
 
-
-#application
-sudo pacman -S --noconfirm yakuake kde-gtk-config ark dolphin dolphin-plugins gwenview kdeconnect neofetch  neovim packagekit-qt5 partitionmanager spectacle kate vlc firefox ktorrent gimp bpytop bitwarden ufw reflector avahi kcalc xdg-utils xdg-user-dirs bash-completion bash-language-server  	openssh kamoso kdenlive kdegraphics-thumbnailers kimageformats libheif qt5-imageformats kdesdk-thumbnailers ffmpegthumbs taglib variety okular kvantum android-tools appmenu-gtk-module libreoffice-fresh elisa
-
-paru -S --noconfirm resvg raw-thumbnailer kde-thumbnailer-apk xdman protonvpn google-chrome systemd-kcm kio-admin qtscrcpy auto-cpufreq
+paru -S --noconfirm resvg raw-thumbnailer kde-thumbnailer-apk xdman protonvpn google-chrome systemd-kcm kio-admin qtscrcpy auto-cpufreq optimus-manager
 
 echo "exec startkde" >> ~/.xinitrc
 sudo ufw allow 1714:1764/udp
@@ -28,6 +27,6 @@ sudo ufw allow 1714:1764/tcp
 sudo systemctl enable sddm
 sudo systemctl enable reflector.timer
 sudo systemctl enable auto-cpufreq
-sudo systemctl enable power-profiles-daemon.service
+sudo systemctl enable optimus-manager.service
 sudo systemctl enable avahi-daemon
 sudo systemctl enable bluetooth
